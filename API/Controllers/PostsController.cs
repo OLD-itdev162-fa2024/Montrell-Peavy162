21eec0e1541c28422020c3f7fb226bc53500671b
Montrell <peavymj@gmatc.matc.edu>
2023-10-22 22:40:57 -0500

    Completed Activity 6

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -20,11 +20,84 @@ namespace API.Controllers
             this.context = context;
         }
 
-
+        /// <summary>
+        /// Get api/posts
+        /// </summary>
+        /// <returns>A list of posts</returns>
         [HttpGet(Name = "GetPosts")]
         public ActionResult<List<Post>> Get()
         {
                 return this.context.Posts.ToList();
+        }
+        ///summary 
+        ///GET api/post/[id]
+        ///</summary>
+        /// <param name="id">Post id</param>
+        /// <returns>A single post</returns>
+        [HttpGet("{id}", Name = "GetById")]
+        public ActionResult<Post> GetById(Guid id)
+        {
+            var post = this.context.Posts.Find(id);
+            if (post is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(post);
+        }
+        ///summary
+        /// POST api/post
+        /// </summary>
+        /// <param name="request">JSON request containing post fields</param>
+        /// <returns> A new post</returns>
+        [HttpPost(Name = "Create")]
+        public ActionResult<Posts> Create([FromBody]Posts request)
+        {
+            var post = new Post
+            {
+                Id = request.Id,
+                Title = request.Title,
+                Body = request.Body,
+                Date = request.Date 
+            };
+
+            context.Posts.Add(post);
+            var success = context.SaveChanges() > 0;
+
+            if (success)
+            {
+                return Ok(post);
+            }
+
+            throw new Exception("Error creating post");
+        }
+         ///summary
+        /// PUT api/post
+        /// </summary>
+        /// <param name="request">JSON request containing one or more updated post fields</param>
+        /// <returns> A new post</returns>
+        [HttpPost(Name = "Update")]
+        public ActionResult<Posts> Update([FromBody]Posts request)
+        {
+            var post = context.posts.Find(request.Id);
+            if(post == null)
+            {
+                throw new Exception("Could not find post"); 
+            }
+
+
+            post.Title = request.Title != null ? request.Title : post.Title;
+            post.Body = request.Body != null ? request.Body : post.Body;
+            post.Date = request.Date != DateTime.MinValue ? request.Date : post.Date;
+
+            var success = context.SaveChanges() > 0;
+                
+            if (success)
+            {
+                return Ok(post);
+            }
+
+            throw new Exception("Error updating post");
         }
     }
 }
